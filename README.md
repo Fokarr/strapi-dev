240fe29a09367f55b7326cd4116a8ab0a7f05693
Fokarr <63513112+Fokarr@users.noreply.github.com>
2020-08-07 20:33:13 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# strapi-dev
+# Strapi application
+
+A quick description of your strapi application
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '33cb6d29-a50e-4d8a-aee7-1db2a008cce2'
+};
